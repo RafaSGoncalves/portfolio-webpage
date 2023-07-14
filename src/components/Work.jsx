52ede3979962027d63609875df8a2a57a13dcdb1
RafaSGoncalves <rafaelnity@yahoo.com.br>
2023-07-14 16:30:06 -0300

    atualizacao do site

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import gettogetherimg from '../assets/gettogetherimg.png'
+import capturadetelanetflix from '../assets/capturadetelanetflix.png';
+import capturadetelatravel from '../assets/capturadetelatravel.png';
 
 const Work = () => {
   return (
@@ -14,12 +16,10 @@ const Work = () => {
           className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
           <div
             style={{backgroundImage: `url(${gettogetherimg})`}}
-            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
-          >
-
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
             <div className='opacity-0 group-hover:opacity-100'>
               <span className='text-2xl font-bold text-white tracking-wider'>
-    Web Application
+                Web Application
               </span>
               <div className='pt-8 text-center'>
                 <a href="http://www.get-together.me">
@@ -28,7 +28,40 @@ const Work = () => {
                 <a href="https://github.com/fabiosimoesds/get-together">
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                 </a>
-
+              </div>
+            </div>
+          </div>
+          <div
+            style={{backgroundImage: `url(${capturadetelanetflix})`}}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                Web Application
+              </span>
+              <div className='pt-8 text-center'>
+                <a href="https://netflix-react-a27d3.web.app">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                </a>
+                <a href="https://github.com/RafaSGoncalves/netflix">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{backgroundImage: `url(${capturadetelatravel})`}}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                Web Application
+              </span>
+              <div className='pt-8 text-center'>
+                <a href="https://travel-react-tailwind.vercel.app">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                </a>
+                <a href="https://github.com/RafaSGoncalves/travel-react-tailwind">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                </a>
               </div>
             </div>
           </div>
